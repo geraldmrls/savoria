f4a07297e47cd56a9dccb8f9de9a3098b6a362ae
Gerald Morales <85576040+geraldmrls@users.noreply.github.com>
2026-04-14 17:23:07 -0600

    Make header lg: version responsive and remove bottom navbar when lg:

--- src/components/BottomNavBar/BottomNavBar.tsx
+++ src/components/BottomNavBar/BottomNavBar.tsx
@@ -1,6 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
+// utils
+import { cn } from "../../utils/cn.ts";
+
 import HomeIcon from "../../assets/home-icon.svg?react";
 import MenuIcon from "../../assets/menu-icon.svg?react";
 import ShowcaseIcon from "../../assets/star-icon.svg?react";
@@ -13,7 +16,13 @@ export function BottomNavBar() {
   }, [pathname]);
 
   return (
-    <nav className="mt-10 flex justify-between px-8 py-2 bg-navbar-bg rounded-tr-2xl rounded-tl-2xl fixed bottom-0 left-0 right-0 z-50 shadow-[0_-4px_6px_rgba(0,0,0,0.1)]">
+    <nav className={cn(
+      //mobile styles
+      "mt-10 flex justify-between px-8 py-2 bg-navbar-bg rounded-tr-2xl rounded-tl-2xl fixed bottom-0 left-0 right-0 z-50 shadow-[0_-4px_6px_rgba(0,0,0,0.1)]",
+
+      //desktop styles - mine
+      "md:hidden lg:hidden xl:hidden 2xl:hidden"
+    )}>
 
       <Link to="/">
         <button className={`py-2 px-5 rounded-full flex flex-col items-center gap-1 ${pathname === "/" ? "bg-primary-bg" : ""} ${pathname === "/" ? "text-yellow-border" : "text-gray-500"}`}>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,84 @@
-import "./Header.css"
+import { NavLink } from "react-router-dom";
+
+// utils
+import { cn } from "../../utils/cn.ts";
+
+import "./Header.css";
 
 export function Header() {
-    return (
-        <header className="bg-primary-bg/95 py-4 px-4 sm:px-5 text-center shadow-md fixed w-full top-0 left-0 z-50 backdrop-blur-sm">
-            <h1 className="text-xl font-semibold">Savoria</h1>
-        </header>
-    )
+  return (
+    <header
+      className={cn(
+        // mobile styles
+        "bg-primary-bg/95 py-4 px-4 sm:px-5 text-center shadow-md fixed w-full top-0 left-0 z-50 backdrop-blur-sm flex justify-center",
+
+        // desktop styles
+        "lg:py-9 lg:px-45 lg:text-left flex lg:items-center lg:justify-between",
+      )}
+    >
+      <NavLink to="/">
+        <h1
+          className={cn(
+            // mobile styles
+            "text-xl font-semibold",
+
+            // desktop styles - mine
+            "lg:text-brown-bg-dark lg:text-4xl lg:font-normal",
+          )}
+        >
+          Savoria
+        </h1>
+      </NavLink>
+
+      {/* navigation links - desktop only */}
+      <div
+        className={cn(
+          // mobile styles
+          "hidden",
+
+          // desktop styles - mine
+          "lg:flex lg:gap-8 lg:items-center lg:select-none",
+        )}
+      >
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            cn(
+              "text-base hover:text-yellow-border cursor-pointer",
+              isActive &&
+                "underline decoration-brown-bg decoration-2 underline-offset-4 text-brown-bg",
+            )
+          }
+        >
+          HOME
+        </NavLink>
+
+        <NavLink
+          to="/menu"
+          className={({ isActive }) =>
+            cn(
+              "text-base hover:text-yellow-border cursor-pointer ",
+              isActive &&
+                "underline decoration-brown-bg decoration-2 underline-offset-4 text-brown-bg",
+            )
+          }
+        >
+          MENU
+        </NavLink>
+
+        <NavLink
+          to="/showcase"
+          className={({ isActive }) =>
+            cn(
+              "text-base hover:text-yellow-border cursor-pointer",
+              isActive &&
+                "underline decoration-brown-bg decoration-2 underline-offset-4 text-brown-bg",
+            )
+          }
+        >
+          SHOWCASE
+        </NavLink>
+      </div>
+    </header>
+  );
 }
