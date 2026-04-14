2bec86f3b31bd8d84c209e8748ef85d5606cf6da
Gerald Morales <85576040+geraldmrls@users.noreply.github.com>
2026-04-13 18:27:25 -0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+
+import './App.css'
+import { Home } from './pages/Home/Home'
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} >
+        {/* pending other pages */}
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+export function Header() {
+    return (
+        <header className="bg-primary-bg py-4 px-5 text-center shadow-md">
+            <h1>Savoria</h1>
+        </header>
+    )
+}
